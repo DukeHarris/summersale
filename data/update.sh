b3130e347e4c24599b831d17aedc21c5dc918ef9
Pedro <pedroflemming@gmail.com>
2014-08-05 05:59:42 +0200

    added csv update script

---
+++ data/update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget "https://docs.google.com/feeds/download/spreadsheets/Export?key=1t6PEl4eYtDXrC8n0umCqU4ruy1mlYRGquLbf7SgRy7I&exportFormat=csv&gid=0" -O /home/torpedro/web/summersale/data/stuff.csv
