e316cc8d3131e2bcb233d93c056508b7adb0d57b
Pedro <pedroflemming@gmail.com>
2014-08-19 23:50:59 +0200

    added gitpull to update.sh

--- data/update.sh
+++ data/update.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+cd  /home/torpedro/web/summersale/
+git pull
+
 wget "https://docs.google.com/feeds/download/spreadsheets/Export?key=1t6PEl4eYtDXrC8n0umCqU4ruy1mlYRGquLbf7SgRy7I&exportFormat=csv&gid=0" -O /home/torpedro/web/summersale/data/stuff.csv
