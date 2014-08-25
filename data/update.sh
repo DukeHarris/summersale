bd50a21b12287ea7290b2e5333d31d4340b798fb
Marvin Keller <marvin.keller01@sap.com>
2014-08-25 13:45:44 -0700

    made update script generic; switched to demo spreadsheet

--- data/update.sh
+++ data/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd  /home/torpedro/web/summersale/
+SPREADSHEETID="19hAbGD_VBhD5i8Fuwt-Rey8jFb3a0MsgmhVLF1FQTQk"
+
+cd "`dirname $0`"
 git pull
 
-wget "https://docs.google.com/feeds/download/spreadsheets/Export?key=1t6PEl4eYtDXrC8n0umCqU4ruy1mlYRGquLbf7SgRy7I&exportFormat=csv&gid=0" -O /home/torpedro/web/summersale/data/stuff.csv
+wget "https://docs.google.com/feeds/download/spreadsheets/Export?key=$SPREADSHEETID&exportFormat=csv&gid=0" -O "$(dirname $0)/stuff.csv"
